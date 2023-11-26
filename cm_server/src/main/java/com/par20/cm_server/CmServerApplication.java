81635ad7ec471d293286dc8f64be091c7c7f3bfd
Charles Lamarque <charles.lamarque69@gmail.com>
2023-11-26 10:44:53 +0100

    [ADD] Spring Boot Initializr project

---
+++ cm_server/src/main/java/com/par20/cm_server/CmServerApplication.java
@@ -0,0 +1,13 @@
+package com.par20.cm_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmServerApplication.class, args);
+	}
+
+}
---
+++ cm_server/src/test/java/com/par20/cm_server/CmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.par20.cm_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
