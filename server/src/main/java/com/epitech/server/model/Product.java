816183f6131cae9a93f5b0f82c842da19055fef1
Charles Lamarque <charles.lamarque69@gmail.com>
2023-12-02 23:17:52 +0100

    [ADD] product model

---
+++ server/src/main/java/com/epitech/server/model/Product.java
@@ -0,0 +1,45 @@
+package com.epitech.server.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("product")
+public class Product {
+
+  @Id
+  private String id;
+  private String name;
+  private float price;
+  private String code;
+
+  public Product(String id, String name, float price, String code) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.code = code;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public float getPrice() {
+    return price;
+  }
+
+  public void setPrice(float price) {
+    this.price = price;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+}
