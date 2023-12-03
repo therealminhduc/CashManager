5aa12cea920aea9d9b595e7a4e9a157bc6b30de1
Charles Lamarque <charles.lamarque69@gmail.com>
2023-12-03 15:46:10 +0100

    [FIX] Repository annotation on top of repository interfaces

--- server/src/main/java/com/epitech/server/repository/UserRepository.java
+++ server/src/main/java/com/epitech/server/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.epitech.server.repository;
 
 import com.epitech.server.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 }
