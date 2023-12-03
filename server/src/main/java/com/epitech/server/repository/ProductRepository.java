85b7bb6cd53c60a2c6e644ab56f23986b8969748
Charles Lamarque <charles.lamarque69@gmail.com>
2023-12-03 13:49:45 +0100

    [ADD] product repository

---
+++ server/src/main/java/com/epitech/server/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.epitech.server.repository;
+
+import com.epitech.server.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+  Product findByName(String name);
+  Product findByNameAndPrice(String truc);
+}
