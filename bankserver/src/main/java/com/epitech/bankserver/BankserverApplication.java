9a4d787797f23e127d16c153c86e1e84867cb28c
therealminhduc <ducbv1407@gmail.com>
2023-12-12 21:40:09 +0100

    [ADD] .env

--- bankserver/src/main/java/com/epitech/bankserver/BankserverApplication.java
+++ bankserver/src/main/java/com/epitech/bankserver/BankserverApplication.java
@@ -1,6 +1,7 @@
 package com.epitech.bankserver;
 
 import com.epitech.bankserver.repository.account.AccountRepository;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class BankserverApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        String mongoDbUri = dotenv.get("MONGODB_URI");
+        String mongoDbName = dotenv.get("MONGODB_NAME");
+
+        System.setProperty("spring.data.mongodb.uri", mongoDbUri);
+        System.setProperty("spring.data.mongodb.database", mongoDbName);
+
         SpringApplication.run(BankserverApplication.class, args);
     }
 
