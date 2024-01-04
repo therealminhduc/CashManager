ff2e2c4ef33b2c83260af7de6e314fa2d49ca6ac
Chun Lam <Chun01@hotmail.fr>
2024-01-04 23:16:00 +0100

    [ADD] RegisterScreen & [FIX] Login screen Input

--- CashManagerInterface/app/src/main/java/com/epitech/cashmanagerinterface/features/user/register/RegisterScreen.kt
+++ CashManagerInterface/app/src/main/java/com/epitech/cashmanagerinterface/features/user/register/RegisterScreen.kt
@@ -3,21 +3,32 @@ package com.epitech.cashmanagerinterface.features.user.register
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,11 +39,20 @@ import com.epitech.cashmanagerinterface.common.navigation.resources.NavItem
 import com.epitech.cashmanagerinterface.ui.theme.lightBlue
 import com.epitech.cashmanagerinterface.ui.theme.lightGray
 import com.epitech.cashmanagerinterface.ui.theme.lightWhite2
-
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
 @Composable
 fun RegisterScreen(navController: NavController) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var isValidUsername by remember {
+        mutableStateOf(false)
+    }
+
+    var isValidPassword by remember {
+        mutableStateOf(false)
+    }
 
     Scaffold(
         topBar = { TopAppBar(NavItem.Register.label) { navController.navigate(NavItem.Login.route) } }
@@ -42,32 +62,79 @@ fun RegisterScreen(navController: NavController) {
                 .fillMaxSize()
                 .background(lightWhite2)
                 .wrapContentSize(Alignment.Center)
+                .padding(16.dp)
         ) {
             Text(
                 text = "Register",
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
-                modifier = Modifier.align(Alignment.CenterHorizontally),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 16.dp),
                 textAlign = TextAlign.Center,
                 fontSize = 20.sp
             )
 
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                label = { Text("Username *") },
+                placeholder = { Text("Enter your username") },
+                value = username,
+                onValueChange = { input ->
+                    username = input
+                    isValidUsername = input.isNotEmpty() },
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                label = { Text("Password *") },
+                placeholder = { Text("Enter your password") },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                value = password,
+                onValueChange = { input ->
+                    password = input
+                    isValidPassword = input.isNotEmpty() },
+                visualTransformation = PasswordVisualTransformation()
+
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                label = { Text("Confirm Password *") },
+                placeholder = { Text("Confirm your password") },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                value = confirmPassword,
+                onValueChange = { input -> confirmPassword = input },
+                visualTransformation = PasswordVisualTransformation()
+            )
             Button(
                 modifier = Modifier
-                    .width(200.dp)
+                    .fillMaxWidth()
                     .padding(top = 16.dp),
                 shape = RoundedCornerShape(10.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = lightBlue),
                 onClick = {
-                    navController.popBackStack()
+                    if (isValidUsername && isValidPassword && password == confirmPassword) {
+                        /*TODO*/
+                    } else {
+                        /*TODO*/
+                    }
                 }
             ) {
                 Text(
-                    text = "Back to login",
+                    text = "Register",
                     style = MaterialTheme.typography.labelLarge,
                     color = Color.White
                 )
             }
+            }
         }
-    }
 }
